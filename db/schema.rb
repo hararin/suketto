3ee5b1b14a8cac1de0972bf5b30b4d6e20898b96
vagrant <vagrant@localhost.localdomain>
2021-02-26 17:34:27 +0000

    is_bannedカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_26_154023) do
+ActiveRecord::Schema.define(version: 2021_02_26_173202) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2021_02_26_154023) do
     t.string "image_id"
     t.integer "ticket", default: 0, null: false
     t.text "introduction"
+    t.boolean "is_banned", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
